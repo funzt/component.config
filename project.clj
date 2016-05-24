907aa63a0793809981cba08b76401323dd5ca837
Leon Grapenthin <grapenthin.leon@gmail.com>
2016-05-24 19:29:41 +0200

    deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.funzt/component.config "0.1.1"
+(defproject org.funzt/component.config "0.1.2-SNAPSHOT"
   :description "Configuration framework for component systems"
   :url "http://github.com/funzt/component.config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha1"]
                  [com.stuartsierra/component "0.3.1"]])
